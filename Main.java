d6f8a4178a715d020df21db9f64b30dee3fcfb98
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-02-21 12:25:48 +0100

    opgave 1 done

--- Main.java
+++ Main.java
@@ -1,16 +1,24 @@
 public class Main {
 	
-public static void main(String args[]){
+	public static void main(String args[]){
 	
-	emptyLine();
-}
+		emptyLine();
+		myString("Hello World");
+		myOtherString("Lucas", 21);
+	}
 
-void emptyLine() {
-System.out.println("");
-}
-
-
-
+	static void emptyLine() {
+		System.out.println("");
+	}
+	static void myString(String tmpString){
+		String stringToPrint = tmpString;
+		System.out.println(stringToPrint);
+	}
+	static void myOtherString(String tmpname, int tmpage){
+		String name = tmpname;
+		int age = tmpage;
+		System.out.println("My name is "+ name + ", i am " + age + " years old.");
+	}
 
 
 
