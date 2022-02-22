3f0d55ce58a08e53ca9b3cb75c857cebf7a70d7c
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-02-22 14:56:02 +0100

    opgave 3 done

---
+++ Student.java
@@ -0,0 +1,28 @@
+public class Student{
+
+String name;
+int age;
+boolean isFemale;
+int datamatikerTeam;
+
+public Student(String tempName, int tempAge, boolean tempIsFemale, int tempDatamatikerTeam) {
+
+name = tempName;
+age = tempAge;
+isFemale = tempIsFemale;
+datamatikerTeam = tempDatamatikerTeam;
+}
+
+
+
+public static void main(String args[]){
+	
+}
+
+
+
+
+
+
+
+}
---
+++ Teacher.java
@@ -0,0 +1,25 @@
+public class Teacher{
+	
+	String name;
+	int age;
+	boolean isFemale;
+
+
+public Teacher(String tmpName, int tmpAge, boolean tmpIsFemale){
+
+name = tmpName;
+age = tmpAge;
+isFemale = tmpIsFemale;
+}
+
+public static void main(String args[]){
+	
+}
+
+
+
+
+
+
+
+}
