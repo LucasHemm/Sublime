44d60cba4ce8fffe156f421503f627baaa8cdbf0
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-02-26 17:33:20 +0100

    opgave 3 done

---
+++ torsdagsOpgaverSæt2/TaskThree.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+public class TaskThree{
+
+
+static int[] myArray = {1, 6423, 64, 6, 3, 123, 61, 6, 36, 1236, 347, 3, 1326, 246, 4};
+
+
+
+public static void main(String[] args){
+System.out.println(Arrays.toString(myArray));
+Arrays.sort(myArray);
+System.out.println(Arrays.toString(myArray));
+
+}
+
+
+
+}
